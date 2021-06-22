8c390ffba0cd7ca4b7aaa105def7ee1c17f4371b
YJRonin <67166948+YJRonin@users.noreply.github.com>
2021-06-22 20:44:27 +0000

    finish project

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 // Author: Ronin YJ
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
 
 // Global ML Variables
-//let mobilenet;
+let mobilenet;
+let video;
+let isModelReady;
 
 function setup() {
-
+  //create UI
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model loading, please wait...")
+  textP.parent(textDiv);
+  // begin capturing webcam video
+  video = createCapture(VIDEO);
+  video.style("display", "none");
+  // initialize ml Variables
+  isModelReady = false;
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
 
 function draw() {
-
+  image(video, 0, 0);
+  if(isModelReady) {
+    mobilenet.classify(canvas, gotResults);
+  }
 }
 
 function modelReady() {
-
+  isModelReady = true;
 }
 
 function gotResults(error, results) {
-
+  if(error) {
+    console.error(error);
+  } else {
+    let label = results[0].label;
+    let confidence = round(results[0].confidence, 2)
+    textP.html("label: " + label + " - confidence " + confidence);
+  }
 }
